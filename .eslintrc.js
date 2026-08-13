46a71e45811fe4d0a9f17741d0057acacc62a32b
Matt Hobbs <matthew.hobbs@hmcts.net>
2026-08-13 15:19:12 +0100

    Update babel monorepo to v8 (@babel/core, @babel/eslint-parser, @babel/preset-env, @babel/runtime, @babel/traverse)

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,6 @@ module.exports = {
       'env': { 'browser': true, 'es6': true, 'node': true },
       'extends': [
         'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
       ],
       'globals': { 'Atomics': 'readonly', 'SharedArrayBuffer': 'readonly' },
